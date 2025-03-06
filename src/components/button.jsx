1eeb87c592400a4a8d6f0bb63bca09eeb0652749
Nandhini Murugan <nandz1407@gmail.com>
2025-03-06 11:12:33 +0530

    completed

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,8 @@
 // write button card here
+import React from 'react';
+const Button = () => {
+    return (
+        <button className="button">Add </button>
+    );
+};
+export default Button; 
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,21 @@
 // write product card here
+import React from 'react';
+import Button from "../components/button";
+import "../index.css"; 
+
+const ProductCard = () => {
+    const productImage="./src/assets/anarkali.jpeg";
+    const productName="Anarkali";
+    const productPrice="Rs 999";
+
+    return (
+        <div className="product-card">
+            <img id='image' src={productImage} alt="product" />
+            <h2>{productName}</h2>
+            <p>{productPrice}</p>
+            <Button />
+         
+        </div>
+    );
+}
+export default ProductCard ;
